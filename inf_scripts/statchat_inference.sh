b72d21dc7574a05cc6e8d04ad371c0aea19b1144
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-26 07:42:43 +0800

    Add inference scripts

---
+++ inf_scripts/statchat_inference.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+accelerate launch src/train_bash.py \
+    --stage sft \
+    --do_predict \
+    --model_name_or_path /data/models/Baichuan2-13B-chat-v2 \
+    --adapter_name_or_path saves/statchat/lora \
+    --dataset statchat_identity,statistics_dataset,deeplearning_dataset,mathematical_statistics_dataset,machine_learning_dataset \ \
+    --dataset_dir data \
+    --template default \
+    --finetuning_type lora \
+    --output_dir saves/statchat/predict \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --cutoff_len 4096 \
+    --preprocessing_num_workers 16 \
+    --per_device_eval_batch_size 8 \
+    --predict_with_generate
+    
