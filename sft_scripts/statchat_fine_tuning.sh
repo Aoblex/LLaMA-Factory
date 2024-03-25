24d6b374583858c5fa1ea41454652d395668f6d9
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-03-25 23:52:02 +0800

    Update statchat fine tuning script

--- sft_scripts/statchat_fine_tuning.sh
+++ sft_scripts/statchat_fine_tuning.sh
@@ -4,14 +4,14 @@ accelerate launch src/train_bash.py \
     --stage sft \
     --do_train \
     --model_name_or_path /data/models/Baichuan2-13B-chat-v2 \
-    --dataset statchat_identity,deeplearning_dataset,machine_learning_dataset,statistics_dataset,mathematical_statistics_dataset \
+    --dataset statchat_identity,statistics_dataset,deeplearning_dataset,mathematical_statistics_dataset,machine_learning_dataset \
     --template default \
     --finetuning_type lora \
     --lora_target W_pack \
     --output_dir saves/statchat/lora \
     --overwrite_output_dir \
     --overwrite_cache \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
