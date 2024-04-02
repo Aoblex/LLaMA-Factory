5ce482334a64c363abc96e23490306ed858d98a2
Chenrui Wang <2021111664@stu.sufe.edu.cn>
2024-04-03 07:31:09 +0800

    Add scripts for statchat knowtuning

--- sft_scripts/statchat_fine_tuning.sh
+++ sft_scripts/statchat_fine_tuning.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+cd statchat-data
+python ./src/rewrite_triplets.py --output_path datasets/know_tuning/statchat_triplets.json \
+                                 --stable_temperature 0.01 \
+                                 --unstable_temperature 0.3 \
+
+python ./src/export_triplets.py
+cp datasets/fine_tuning_data/statchat_KC.json ../data/statchat_KC.json
+cp datasets/fine_tuning_data/statchat_KG.json ../data/statchat_KG.json
+cd ..
+
 accelerate launch src/train_bash.py \
     --ddp_timeout 18000000 \
     --stage sft \
     --do_train \
     --model_name_or_path /data/models/Baichuan2-13B-chat \
-    --dataset statchat_identity,knowledge_dataset,statistics_dataset,deeplearning_dataset,mathematical_statistics_dataset,machine_learning_dataset \
+    --dataset statchat_identity,statchat_KG \
     --template default \
     --finetuning_type lora \
     --lora_target W_pack \
-    --output_dir saves/statchat/lora-v4 \
+    --output_dir saves/statchat/knowtuning/sft \
     --overwrite_output_dir \
     --overwrite_cache \
     --per_device_train_batch_size 8 \
@@ -16,10 +26,37 @@ accelerate launch src/train_bash.py \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 1000 \
-    --learning_rate 6e-4 \
+    --learning_rate 5e-5 \
     --num_train_epochs 3.0 \
     --plot_loss \
-    --lora_rank 8 \
-    --lora_alpha 12 \
+    --fp16 \
+
+CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+    --stage dpo \
+    --do_train \
+    --model_name_or_path /data/models/Baichuan2-13B-chat \
+    --adapter_name_or_path saves/statchat/knowtuning/sft \
+    --create_new_adapter \
+    --dataset statchat_KC \
+    --template default \
+    --finetuning_type lora \
+    --lora_target W_pack \
+    --output_dir saves/statchat/knowtuning/dpo \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --warmup_steps 20 \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --load_best_model_at_end \
+    --learning_rate 1e-5 \
+    --num_train_epochs 1.0 \
+    --dpo_ftx 1.0 \
+    --plot_loss \
     --fp16
-    
+
+python test_output.py
